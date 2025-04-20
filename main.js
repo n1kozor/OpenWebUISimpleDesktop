69bb41fa00353c366452417bd95e5af9a299775a
_00_ <131402327+rgaricano@users.noreply.github.com>
2025-04-20 03:21:47 +0200

    UPD: Linux install
    Modification for Linux installer
    
    Changes:
    
    - added email (required for compile)
    - change icon format to png (if not error)
    - change config directory to appData (it point to: %APPDATA% on Windows
    $XDG_CONFIG_HOME or ~/.config on Linux ~/Library/Application Support on
    macOS), with userData (app installer directory) there are write
    permissions problems on linux.
    - updated electron libs.

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ const os = require('os');
 const http = require('http');
 
 const isPackaged = app.isPackaged;
-const configDir = app.getPath('userData');
+const configDir = app.getPath('appData');
 const configPath = path.join(configDir, 'config.json');
 let mainWindow = null;
 let modalWindow = null;
